6142534a7b14d1722365f7fc1dd3aebad1bf2e89
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-10-29 19:31:08 +0530

    server

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express =require("express");
 const convertCode = require("./Routes/Convert");
 require('dotenv').config();
-const cors=require("cors")
+const cors = require('cors');
 
 
 const app=express()
 const PORT = process.env.PORT || 8000;
 app.use(express.json())
 app.use("/",convertCode)
-app.use(cors())
+app.use(cors());
 
 
 app.listen(PORT, () => {
